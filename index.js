26bfacaa13e604c591037142c8e15610a1d17f40
KathitShah <49078541+KathitShah@users.noreply.github.com>
2020-04-10 23:19:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function hi(){
+  var list = ["Abby", "Alyssa", "Aziz", "Camilla", "Catherine", "Draper", "Charles", "Elizabeth", "Elly", "Emili", "Ethan", "John", "Jaypee", "Jp", "Jonathan", "John paul", "Kathit", "Kato", "Naima", "Maxwell", "Max", "Lee", "Kushal", "Khyle", "Peter", "Asher", "Hiren", "Asher", "A", "Trevor", "Sydney", "Steven", "Simran", "Shamel"]
   var password = ["kathitisthebest"];
   var myName = document.getElementById("demo").value;
   var letter = myName.slice(0,1);
@@ -7,7 +8,7 @@ function hi(){
   var letters = myName.slice(1,myName.length);
   var noCapital = letters.toLowerCase();
   var yourName = capitalize + noCapital;
-  if(password.includes(myPassword)){
+  if(list.includes(yourName) && password.includes(myPassword)){
     if(yourName === "Catherine" && password.includes(myPassword)|| yourName === "Draper" && password.includes(myPassword)){
       alert("Welcome Mrs.Draper, you are being redirected to my real website!!!");
       var change = document.getElementById("big-btn").href="real.html";
